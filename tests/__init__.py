d6905d122779cceb56b25dff08bdeaa4cb38091f
Carlos G?lvez <carlos.galvez@nan-tic.com>
2019-09-26 14:00:43 +0200

    Upgrade setup and tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from .test_edocument_unedifact import suite
+try:
+    from trytond.modules.edocument_unedifact.tests.test_edocument_unedifact import suite
+except ImportError:
+    from .test_edocument_unedifact import suite
+
+__all__ = ['suite']
